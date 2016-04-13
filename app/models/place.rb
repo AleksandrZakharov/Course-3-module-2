b48a695dd3c31da8bd4baf6baef3116bf6be2067
human.of.rain <hauman.of.rain@gmail.com>
2016-04-13 13:22:43 +0300

    rspec spec/aggregation_spec.rb -e rq02

---
+++ app/models/place.rb
@@ -0,0 +1,88 @@
+class Place
+
+  attr_accessor :id, :formatted_address, :location, :address_components
+
+  def initialize(params)
+
+  	@id = params[:_id].to_s  	
+  	@address_components = []
+  	params[:address_components].each {|comp| @address_components << AddressComponent.new(comp)}  	
+  	@formatted_address = params[:formatted_address]  	
+  	@location = Point.new(params[:geometry][:geolocation])  	
+
+  end
+
+  def self.mongo_client
+
+  	Mongoid::Clients.default
+
+  end
+
+  def self.collection
+
+  	@coll = self.mongo_client['places']
+
+  end
+
+  def self.load_all (io)
+ 
+  	collection.insert_many(JSON.parse(File.read(io)))
+
+  end
+
+  def self.find_by_short_name (string)
+
+    collection.find("address_components.short_name" => string)
+
+  end
+
+  def self.to_places (data)
+
+    result = []
+    data.each { |place| result << Place.new(place) }
+    return result
+
+  end
+
+  def self.find id
+
+    id = BSON::ObjectId.from_string(id)
+    result = collection.find(:_id => id).first
+    result = Place.new(result) if !result.nil?
+  end
+
+  def self.all(offset=0,limit=nil)
+
+    result = []
+    data = collection.find.skip(offset)
+    data = data.limit(limit) if !limit.nil?
+    data.each {|place| result << Place.new(place)}
+    return result
+  end
+
+  def destroy
+
+    puts @id
+    self.class.collection.find(:_id => BSON::ObjectId.from_string(@id)).delete_one
+
+  end
+
+  def self.get_address_components(sort={:_id => 1},offset=0,limit=nil)
+
+    if !limit.nil?
+      data = collection.aggregate( [ { :$unwind => "$address_components" }, {:$project => {:_id => 1, :address_components => 1, :formatted_address => 1, "geometry.geolocation" => 1}}, {:$sort => sort}, {:$skip => offset}, {:$limit => limit}] )
+    else
+      data = collection.aggregate( [ { :$unwind => "$address_components" }, {:$project => {:_id => 1, :address_components => 1, :formatted_address => 1, "geometry.geolocation" => 1}}, {:$sort => sort}, {:$skip => offset}] )
+    end
+    return data
+
+  end
+
+  def self.get_country_names
+
+    data = collection.aggregate([{:$unwind => "$address_components"}, {:$project => {"address_components.long_name" =>1, "address_components.types" => 1}}, {:$match => {"address_components.types" => "country"}}, {:$group => {:_id => "$address_components.long_name"}}]).to_a.map {|h| h[:_id]}
+   
+
+  end
+
+end
---
+++ app/models/point.rb
@@ -0,0 +1,24 @@
+class Point
+	attr_accessor :longitude, :latitude
+
+	def to_hash
+		
+	      jsoncoord = {:type => "Point", :coordinates => [@longitude, @latitude]}
+    
+	end
+
+	def initialize (params)
+
+		coord = {}
+		if params[:lat]
+			@longitude=params[:lng]
+	      	@latitude=params[:lat]
+		else
+	      	@longitude=params[:coordinates][0]
+	      	@latitude=params[:coordinates][1]
+
+    end
+
+	end
+
+end
