f54408a7139290356cd3a6a547167d1ca6a350f8
human.of.rain <hauman.of.rain@gmail.com>
2016-04-15 08:41:46 +0300

    rspec spec/geo_spec.rb -e rq01

--- app/models/place.rb
+++ app/models/place.rb
@@ -82,6 +82,29 @@ class Place
 
     data = collection.aggregate([{:$unwind => "$address_components"}, {:$project => {"address_components.long_name" =>1, "address_components.types" => 1}}, {:$match => {"address_components.types" => "country"}}, {:$group => {:_id => "$address_components.long_name"}}]).to_a.map {|h| h[:_id]}
    
+  end
+
+  def self.find_ids_by_country_code (country_code)
+
+    data = collection.aggregate([{:$match => {"address_components.types" => "country", "address_components.short_name" => country_code}}, {:$project => {:_id => 1}}]).map {|doc| doc[:_id].to_s}
+
+  end
+
+  def self.create_indexes
+
+    collection.indexes.create_one("geometry.geolocation" => Mongo::Index::GEO2DSPHERE)
+
+  end
+
+  def self.remove_indexes
+
+    Place.collection.indexes.drop_one("geometry.geolocation_2dsphere")
+
+  end
+
+  def self.near (point, max_meters=nil)
+
+    
 
   end
 
